daf54978c157a8ecb0bf1262a30c3f5b61f72a52
Timothy Li <143044585+tjl243@users.noreply.github.com>
2025-03-19 00:55:37 -0400

    Update lock_spin.c

--- lock_spin.c
+++ lock_spin.c
@@ -1,1 +1,29 @@
 
+#include <stdlib.h>
+#include "3140_concur.h"
+#include "lock.h"
+
+void l_init(lock_t* l) {
+	__disable_irq();
+	l -> flag = 0;
+	__enable_irq();
+}
+
+void l_lock(lock_t* l) {
+	while(1) {
+		__disable_irq();
+		if(l -> flag == 0) {
+			l -> flag = 1;
+			__enable_irq();
+			return;
+		}
+		__enable_irq();
+		delay(1);
+	}
+}
+
+void l_unlock(lock_t* l) {
+	__disable_irq();
+	l -> flag = 0;
+	__enable_irq();
+}
