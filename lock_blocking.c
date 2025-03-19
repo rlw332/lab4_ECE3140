c7fa935c3c6883d7e0723089ac9f1a946abe6ff5
Timothy Li <143044585+tjl243@users.noreply.github.com>
2025-03-19 00:55:14 -0400

    Update lock_blocking.c

--- lock_blocking.c
+++ lock_blocking.c
@@ -1,1 +1,33 @@
+#include <stdlib.h>
+#include "3140_concur.h"
+#include "lock.h"
 
+void l_init(lock_t* l) {
+	__disable_irq();
+	l -> flag = 0;
+	l -> process_queue -> head = NULL;
+	__enable_irq();
+}
+
+void l_lock(lock_t* l) {
+	//while(1) {
+		__disable_irq();
+		if(l -> flag == 0) {
+			l -> flag = 1;
+			return;
+		} else {
+			enqueue(current_process_p, l -> process_queue);
+			current_process_p -> blocked = 1;
+			__enable_irq();
+			process_blocked();
+		}
+	//}
+}
+
+void l_unlock(lock_t* l) {
+	__disable_irq();
+	l -> flag = 0;
+	process_t blocked_process = dequeue(l -> process_queue);
+	blocked_process -> blocked = 0;
+	__enable_irq();
+}
