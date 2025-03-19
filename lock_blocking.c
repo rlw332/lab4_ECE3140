c39fab3a4bb5095e89da2bcd668041ac9733f409
Timothy Li <143044585+tjl243@users.noreply.github.com>
2025-03-19 01:47:05 -0400

    Update lock_blocking.c

--- lock_blocking.c
+++ lock_blocking.c
@@ -27,7 +27,7 @@ void l_lock(lock_t* l) {
 void l_unlock(lock_t* l) {
 	__disable_irq();
 	l -> flag = 0;
-	current_process_p -> blocked = 1;
+	current_process_p -> blocked = 1; // i think this line is optional (??)
 	enqueue(current_process_p, l -> process_queue);
 	process_t blocked_process = dequeue(l -> process_queue);
 	blocked_process -> blocked = 0;
