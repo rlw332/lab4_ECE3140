c7f085d68634d4414528abb1752a3ec9e36c1a50
Rawsen Weideman <142469376+rlw332@users.noreply.github.com>
2025-03-21 22:24:29 -0400

    What test cases do

--- final code/mytest_1.c
+++ final code/mytest_1.c
@@ -1,7 +1,5 @@
 //TEST CASE 1
-//This test case has to do with what happens when two processes both require two locks (i.e. both processes are doubled locked)
-//For spin lock, we expect the program to first go into p1 
-//For blocking lock, we expect...
+//Comments about behavior at the bottom
 
 #include "3140_concur.h"
 #include "led.h"
@@ -68,7 +66,6 @@ int main(void) {
         return -1;
     }
 
-    //
     process_start();
     delay(300);
     green_toggle_frdm();
@@ -85,3 +82,20 @@ int main(void) {
 
     while (1);
 }
+
+//This test case has to do with what happens when two processes both require 
+//two locks (i.e. both processes are doubled locked)
+
+//For spin lock, we expect the program to first create two locks l1 and l2, and then go into process p1,
+//where the process will lock l1 and then lock l2 after a short delay. Then p1 will enter its critical section 
+//and completes the process of turning on and off the red FRDM led on for a medium, then long, then short amount of time
+//After p1 completes its critical section, it unlocks the locks p1 and p2 and jumps into the p2 program where it 
+//completes the process of toggling on the green FRDM led, and turning on and off the red FRDM twice before finishing
+//its critical section and moving on to the final process in the init where it toggles on the FRDM 
+//the green off and then the red on, and then the green on and then the red off before turning the blue and red LEDS in LED0 on
+
+//For blocking lock, we expect the same program to happen (a little faster), but the process in which the locks are used will vary
+//In this case after the locks l1 and l2 are initializes, p1 will try to lock l1 if it is available (which it is) and then moments later lock l2
+//The process p2 will also try to acquire the locks l1 and l2, but since p1 already has them, p2 will block the locks at the l_lock(&l1) 
+//and l_lock(&l2) function calls, and wait until p1 releases the locks by first completeing its critcial section and then unlocking the locks for use
+//which p2 can now lock, using them to enter the critical section before unlocking and continuing with the processes in the init
