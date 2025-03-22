88b84f0d7c24d9e3092fc842509f546bc0e7c263
Timothy Li <143044585+tjl243@users.noreply.github.com>
2025-03-22 17:43:12 -0400

    Update lock_blocking.c

--- final code/lock_blocking.c
+++ final code/lock_blocking.c
@@ -3,21 +3,22 @@
 #include "lock.h"
 
 void l_init(lock_t* l) {
-	__disable_irq();
-	l -> flag = 0;
-	l -> process_queue = malloc(sizeof(process_queue_t));
-	l -> process_queue -> head = NULL;
+	__disable_irq(); //optional
+	l -> flag = 0; // initialize flag
+	l -> process_queue = malloc(sizeof(process_queue_t)); // allocate memory for the queue in regards to the lock
+	l -> process_queue -> head = NULL; // set queue as empty for now
 	__enable_irq();
 }
 
 void l_lock(lock_t* l) {
-	while(1) {
-		__disable_irq();
-		if(l -> flag == 0) {
+	while(1) { //this loop differs from spin lock as it almost never gets executed unless absolutely necessary (e.g. freed lock or first check)
+		__disable_irq(); // atomicity
+		if(l -> flag == 0) { // if lock is free then acquire and we are done
 			l -> flag = 1;
 			__enable_irq();
 			return;
-		} else {
+		} else { // otherwise add tthis process to the queue for this lock if not already added 
+			// (could already be added if another process 'snipes' the lock immediately after freed if it perfectly asks for lock then)
 			if(current_process_p -> blocked == 0) {
 				enqueue(current_process_p, l -> process_queue);
 				current_process_p -> blocked = 1;
@@ -29,12 +30,10 @@ void l_lock(lock_t* l) {
 }
 
 void l_unlock(lock_t* l) {
-	__disable_irq();
-	l -> flag = 0;
-	//current_process_p -> blocked = 1; // i think this line is optional (??)
-	//enqueue(current_process_p, l -> process_queue);
-	process_t *blocked_process = dequeue(l -> process_queue);
-	if(blocked_process != NULL){
+	__disable_irq(); // optional but good for fairness
+	l -> flag = 0; // free lock
+	process_t *blocked_process = dequeue(l -> process_queue); // get one of blocked processes and unblock it (wake it up)
+	if(blocked_process != NULL){ // need to check if queue was empty because then calling its member could be undefined behavior
 		blocked_process -> blocked = 0;
 		enqueue(blocked_process, &process_queue);
 	}
